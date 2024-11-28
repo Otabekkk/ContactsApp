93b00455e33b2d0c60f296a613131e57ec693fb8
Otabekkk <otabekiusupov1@gmail.com>
2024-11-28 23:16:29 +0600

    Готово.

--- static/script.js
+++ static/script.js
@@ -1,12 +1,3 @@
-// function confirmDelete(contact_id) {
-//     var result = confirm('Вы правда хотите удалить контакт?')
-
-//     if (result) {
-//         window.location.href = 'delete/' + contact_id;
-//     }
-// }
-
-
 function openModal(contact_id) {
     document.getElementById("confirm").style.display = 'block';
 
